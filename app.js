c36406e31d1f2efbd08a625b35e7a5e706911fd4
Neha Chaturvedi <neha.chaturvedi@mozo.com.au>
2021-03-18 19:34:54 +1100

    Add function to handle bad request

--- app.js
+++ app.js
@@ -6,32 +6,47 @@
 
 const express = require('express')
 const app = express()
-const bodyParser = require('body-parser');
+const bodyParser = require('body-parser')
 const sampleReq = require('./request.json')
 
 const port = process.env.PORT || 8000
-app.use(bodyParser.json());
+
+
+app.use(bodyParser.json())
+
+const error = {
+  "error": "Could not decode request: JSON parsing fail"
+}
+
+app.use((err, req, res, next) => {
+  // To handle bad request
+  if (err instanceof SyntaxError && err.status === 400 && 'body' in err) {
+    return res.status(400).send(error); 
+  }
+  next();
+});
 
 app.get('/', (req, res) => {
-  res.status(200).json({
+  return res.status(200).json({
     "message": "This is a rest api to render json response ",
-    "example":  sampleReq
+    "example": sampleReq
   })
 })
 
 app.get('/v1/results', (req, res) => {
-  res.status(200).json({
-    "message": "This is a rest api to render json response ",
-    "example":  sampleReq
-  })
+  if (req)
+    return res.status(200).json({
+      "message": "This is a rest api to render json response ",
+      "example": sampleReq
+    })
 })
 
 app.post('/v1/results', (req, res) => {
-  
+
   try {
     const { payload } = req.body
     const filteredData = payload && payload.filter(p => p.drm && p.episodeCount > 0)
-    
+
     if (filteredData.length > 0) {
       const responseObj = filteredData.map(fd => {
         return {
@@ -46,14 +61,10 @@ app.post('/v1/results', (req, res) => {
     } else {
       // if payload has dm: true && episode>0 and does not have valid image, slug, title
       // then return error response  
-      res.status(400).json({
-        error: 'Could not decode request: JSON parsing fail',
-      });
+      res.status(400).json(error);
     }
   } catch (e) {
-    res.status(400).json({
-      error: 'Could not decode request: JSON parsing fail',
-    });
+    res.status(400).json(error)
   }
 })
 app.listen(port, () => {
